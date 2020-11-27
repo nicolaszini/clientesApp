381ce8947178c64d420a24c0b9a9ea6ebdbb6f25
unknown <nicolaszini8@gmail.com>
2020-11-26 22:13:56 -0300

    first commit

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,11 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre:'Gonzalo', apellido:'Zini', email:'gonzalo@gmail.com', createAt: '2020-12-11'},
+    {id: 2, nombre:'Nicolas', apellido:'Zini', email:'nico@gmail.com', createAt: '2020-06-12'},
+    {id: 3, nombre:'Jorge', apellido:'Zini', email:'jorge@gmail.com', createAt: '2020-07-12'},
+    {id: 4, nombre:'Virginia', apellido:'Zini', email:'virginia@gmail.com', createAt: '2020-02-10'},
+    {id: 5, nombre:'Mariano', apellido:'Zini', email:'mariano@gmail.com', createAt: '2020-12-10'}
+
+];
+    
---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+
+})
+export class DirectivaComponent  {
+
+  listaCurso:string[] = ['TypeScript','Java SE','C','C#'];
+
+  habilitar:boolean = true;
+
+  setHabilitar(): void {
+    this.habilitar = (this.habilitar==true)? false:true;
+    
+  }
+
+
+  constructor() { }
+
+}
