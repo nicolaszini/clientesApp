f5b02f78daf19d11da2d97c76b54b53d9f6671c6
unknown <nicolaszini8@gmail.com>
2020-12-01 16:20:22 -0300

    Connect api - front !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,17 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormComponent } from './clientes/form.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'directivas', component: DirectivaComponent},
-  {path: 'clientes', component: ClientesComponent}
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'clientes/form', component: FormComponent}
 
 ];
 
@@ -21,14 +26,17 @@ const routes: Routes = [
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
 
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,18 +2,28 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable, of } from 'rxjs';
-
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ClienteService {
+	
+	private urlEndPoint:string = 'http://localhost:8080/api/clientes';
+	private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
 
-  constructor() { }
+	constructor(private http: HttpClient) { }
 
-  getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
-  }
+	getClientes(): Observable<Cliente[]> {
+		//return of(CLIENTES);
+		return this.http.get(this.urlEndPoint).pipe(
+			map( response => response as Cliente[])
+		);
+	}
+
+	create(cliente: Cliente) : Observable<Cliente>  {
+		return this.http.post<Cliente>(this.urlEndPoint, cliente,{headers: this.httpHeaders})  
+	}
 
 }
