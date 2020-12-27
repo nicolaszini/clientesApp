63bf44106d2a3c359bbe8c816cc9af9013e20a5c
unknown <nicolaszini8@gmail.com>
2020-12-27 10:29:21 -0300

    Seccion 13: upload imagen

--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -45,6 +45,8 @@ export class DetalleComponent implements OnInit {
         } else if(event.type === HttpEventType.Response) {
           let response:any = event.body;
           this.cliente = response.cliente as Cliente;
+
+          this.modalService.notificarUpload.emit(this.cliente);
           swal('La foto se ha subido completamente!', response.mensaje, 'success');
         }        
       });
